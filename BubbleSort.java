1ac31d3dace0d9df2c29b2b668da0232f48bd5d3
Sai bharath <jambulasaibharath123@gmail.com>
2025-07-17 11:04:04 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -17,8 +17,8 @@ class BubbleSort
             for(j=0;j<n-1-i;j++){
                 if(a[j]>a[j+1]){
                     t = a[j];
-                a[j] = a[j+1];
-                a[j+1]=t;
+                    a[j] = a[j+1];
+                    a[j+1]=t;
                 }
             }
         }
